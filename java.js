b5d62b3ef56d80ad19598db8ae886a1760a2d39c
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-10-10 15:03:41 +0800

    add hover effect

--- java.js
+++ java.js
@@ -1,5 +1,14 @@
 const container = document.querySelector("#container");
 
+const buttonContainer = document.createElement("div");
+container.appendChild(buttonContainer);
+
+const button = document.createElement("button");
+button.textContent = "GRID";
+button.style.marginBottom = "5px";
+buttonContainer.appendChild(button);
+
+
 function createRowOfSquares(num){
     for (let i = 0; i < num; i++){
         const row = document.createElement("div");
@@ -14,3 +23,25 @@ function createRowOfSquares(num){
 }
 
 createRowOfSquares(16);
+
+
+const gridDivs = document.querySelectorAll(".box");
+
+
+gridDivs.forEach((sq) => {
+
+    sq.addEventListener("mouseenter", (e) => {
+    e.target.style.background = "";
+});
+
+    sq.addEventListener("mouseleave", (e) => {
+    e.target.style.background = "pink";
+});
+
+});
+
+
+button.addEventListener("click", () => {
+    //let num = +prompt("How many squares would you like per side?", "2-100");
+    
+});
